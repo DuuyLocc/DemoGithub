7f3d7571a9c018f9473ecf03366345b375d7a9e3
Lucas Le <duuylocc@outlook.com.vn>
2020-10-13 14:51:01 +0700

    create solution structure (2 new class)

---
+++ lucasitstore.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace lucasitstore.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ lucasitstore.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace lucasitstore.Data
+{
+    public class Class1
+    {
+    }
+}
